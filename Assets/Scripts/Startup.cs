2692e560f690423191d482c1de6cb587f12317a8
Christopher Head <chead@chead.ca>
2021-06-12 10:48:49 -0700

    Eliminate no cameras warning message

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -1,28 +1,39 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Startup : MonoBehaviour {
 	private const string rootScene = "Root";
 	private const string firstLevel = "Level01";
+	private GameObject rootCamera;
+	private bool loadInProgress = false;
 
     void Start() {
+		rootCamera = GameObject.Find("Root Camera");
 		if(SceneManager.sceneCount == 1) {
 			// This is the normal game startup path. Only the root scene is
 			// loaded, so start loading the first level.
 			StartLoad(firstLevel);
-		} else if(SceneManager.GetActiveScene().name == rootScene) {
-			// This is a debug path where the active scene was set to root, but
-			// another scene is also loaded. Activate the other scene; if there
-			// are more than one, activate one and unload the others.
-			bool anyActivated = false;
-			for(int i = 0; i != SceneManager.sceneCount; ++i) {
-				Scene scene = SceneManager.GetSceneAt(i);
-				if(scene.name != rootScene) {
-					if(!anyActivated) {
-						SceneManager.SetActiveScene(scene);
-						anyActivated = true;
-					} else {
-						SceneManager.UnloadSceneAsync(scene);
+		} else {
+			// Another scene is loaded in addition to the root scene.
+			// Deactivate the root camera and let the other scene’s camera take
+			// over.
+			rootCamera.SetActive(false);
+			if(SceneManager.GetActiveScene().name == rootScene) {
+				// This is a debug path where the active scene was set to root,
+				// but another scene is also loaded. Activate the other scene;
+				// if there are more than one, activate one and unload the
+				// others.
+				bool anyActivated = false;
+				for(int i = 0; i != SceneManager.sceneCount; ++i) {
+					Scene scene = SceneManager.GetSceneAt(i);
+					if(scene.name != rootScene) {
+						if(!anyActivated) {
+							SceneManager.SetActiveScene(scene);
+							anyActivated = true;
+						} else {
+							SceneManager.UnloadSceneAsync(scene);
+						}
 					}
 				}
 			}
@@ -33,23 +44,38 @@ public class Startup : MonoBehaviour {
 		StartLoad(SceneManager.GetActiveScene().name);
 	}
 
-	private void StartLoad(string level) {
+	public void StartLoad(string level) {
+		if(!loadInProgress) {
+			loadInProgress = true;
+			StartCoroutine(Load(level));
+		}
+	}
+
+	private IEnumerator Load(string level) {
 		// Unload any loaded scenes other than the root.
-		for(int i = 0; i != SceneManager.sceneCount; ++i) {
+		for(int i = SceneManager.sceneCount - 1; i >= 0; --i) {
 			Scene scene = SceneManager.GetSceneAt(i);
 			if(scene.name != rootScene) {
-				// A loaded scene was found. Unload it and, when that is done,
-				// go back to StartLoad again.
 				AsyncOperation unloadOp = SceneManager.UnloadSceneAsync(scene);
-				unloadOp.completed += (_) => StartLoad(level);
-				return;
+				yield return unloadOp;
 			}
 		}
 
-		// Load the target scene.
+		// Enable the root camera, because the scene camera has just been
+		// unloaded.
+		rootCamera.SetActive(true);
+
+		// Load the new scene.
 		AsyncOperation loadOp = SceneManager.LoadSceneAsync(level, LoadSceneMode.Additive);
-		loadOp.completed += (_) => {
-			SceneManager.SetActiveScene(SceneManager.GetSceneByName(level));
-		};
+		yield return loadOp;
+
+		// Set the new scene as the active scene.
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName(level));
+
+		// Disable the root camera because the new scene has its own.
+		rootCamera.SetActive(false);
+
+		// Done loading!
+		loadInProgress = false;
     }
 }
