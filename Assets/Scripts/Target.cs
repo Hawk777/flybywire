af4801ae6ea46bbaba5e9870bf9f9771ad559b73
Christopher Head <chead@chead.ca>
2021-06-11 23:59:53 -0700

    Reparent the connector instead of using a joint

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Target : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D collision) {
 		GameObject projectile = collision.gameObject;
 		if(projectile.tag == "Projectile") {
 			ComputerControls launcher = projectile.GetComponent<Projectile>().launcher;
 
-			FixedJoint2D joint = projectile.AddComponent<FixedJoint2D>();
-			joint.connectedBody = GetComponent<Rigidbody2D>();
-			joint.enableCollision = false;
+			// The projectile should stick, perfectly, to the target, and move
+			// with it. To accomplish this, simply destroy its own rigid body
+			// and make it a child object of the target.
+			Destroy(projectile.GetComponent<Rigidbody2D>());
+			projectile.GetComponent<Transform>().SetParent(GetComponent<Transform>(), true);
 
+			// Create the cable between the launcher and the projectile.
 			SpringJoint2D spring = launcher.gameObject.AddComponent<SpringJoint2D>();
 			spring.autoConfigureDistance = true;
 			spring.dampingRatio = 0f;
@@ -18,6 +22,7 @@ public class Target : MonoBehaviour {
 			spring.enableCollision = true;
 			launcher.spring = spring;
 
+			// Record the object that was hit, in case it is activatable.
 			launcher.activationTarget = GetComponent<Activatable>();
 		}
 	}
