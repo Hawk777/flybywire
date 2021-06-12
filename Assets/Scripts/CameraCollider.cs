28e495d29ffd2ce8eb3cf58a6545463450b776c7
Zereijin <zereijin@gmail.com>
2021-06-12 09:30:31 -0700

    Smooth camera retargeting

--- Assets/Scripts/CameraCollider.cs
+++ Assets/Scripts/CameraCollider.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraCollider : MonoBehaviour {
-	public Camera targetCamera;
+	[Tooltip("New location for the camera to move to")]
 	public Vector3 cameraTargetPosition = new Vector3(0, 0, -10);
+
+	[Tooltip("New camera size")]
 	public float cameraTargetSize = 10f;
+
+	private Camera targetCamera;
+	private FollowWithSmoothing targetSmoothing;
+
+	private void Start() {
+		GameObject targetObject = GameObject.Find("Main Camera");
+		targetCamera = targetObject.GetComponentInChildren<Camera>();
+		targetSmoothing = targetObject.GetComponentInChildren<FollowWithSmoothing>();
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.gameObject.tag == "Player") {
-			targetCamera.transform.position = cameraTargetPosition;
-			targetCamera.orthographicSize = cameraTargetSize;
+			if (targetSmoothing) {
+				targetSmoothing.SetNewTarget(cameraTargetPosition, cameraTargetSize);
+			} else {
+				targetCamera.transform.position = cameraTargetPosition;
+				targetCamera.orthographicSize = cameraTargetSize;
+			}
 		}
 	}
 }
--- Assets/Scripts/FollowWithSmoothing.cs
+++ Assets/Scripts/FollowWithSmoothing.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 
 [RequireComponent(typeof(Transform))]
 public class FollowWithSmoothing : MonoBehaviour {
-	[Tooltip("Name of the object that the follower should follow")]
-	public string targetName;
-
 	[Tooltip("Speed at which the follower settles")]
 	public float speed = 2.0f;
 
 	[Tooltip("Distance at which interpolation stops and the follower jumps")]
 	public float jumpDist = 0.1f;
 
-	private Transform target;
+	private Camera targetCamera;
+	private Vector3 targetPosition;
+	private float targetSize;
 
 	void Start() {
-		target = GameObject.Find(targetName).GetComponent<Transform>();
+		targetCamera = GetComponentInChildren<Camera>();
+		targetPosition = transform.position;
+		targetSize = targetCamera.orthographicSize;
 	}
 
 	void Update() {
 		float interpolation = speed * Time.deltaTime;
+
 		Transform me = GetComponent<Transform>();
 		Vector2 oldPos = me.position;
-		Vector2 targetPos = target.position;
+		Vector2 targetPos = targetPosition;
 		Vector2 newPos;
 		if((targetPos - oldPos).sqrMagnitude <= jumpDist * jumpDist) {
 			newPos = targetPos;
@@ -29,5 +31,19 @@ public class FollowWithSmoothing : MonoBehaviour {
 			newPos = Vector2.Lerp(oldPos, targetPos, interpolation);
 		}
 		me.position = new Vector3(newPos.x, newPos.y, me.position.z);
+
+		float oldSize = targetCamera.orthographicSize;
+		float newSize;
+		if ((targetSize - oldSize) <= jumpDist * jumpDist) {
+			newSize = targetSize;
+		} else {
+			newSize = Mathf.Lerp(oldSize, targetSize, interpolation);
+		}
+		targetCamera.orthographicSize = newSize;
+	}
+
+	public void SetNewTarget(Vector3 newTargetPosition, float newTargetSize) {
+		targetPosition = newTargetPosition;
+		targetSize = newTargetSize;
 	}
 }
