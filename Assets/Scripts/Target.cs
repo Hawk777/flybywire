8aa8fb87f7140ee18d6e19356b57bd9b978d6f91
Christopher Head <chead@chead.ca>
2021-06-12 18:42:51 -0700

    Fix duplicate target connection

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -19,32 +19,38 @@ public class Target : MonoBehaviour {
 			// Get the player that launched the projectile.
 			ComputerControls launcher = projectileComponent.launcher;
 
-			// The projectile should stick, perfectly, to the target, and move
-			// with it. To accomplish this, simply destroy its own rigid body
-			// and make it a child object of the target.
-			Destroy(projectile.GetComponent<Rigidbody2D>());
-			projectile.GetComponent<Transform>().SetParent(GetComponent<Transform>(), true);
+			// Normally this will be the case; however, in the event that the
+			// projectile enters the bounding boxes of two different targets in
+			// the same frame, it might not be, and we only want to plug into
+			// the first target.
+			if(launcher.connectedTarget == null) {
+				// The projectile should stick, perfectly, to the target, and
+				// move with it. To accomplish this, simply destroy its own
+				// rigid body and make it a child object of the target.
+				Destroy(projectile.GetComponent<Rigidbody2D>());
+				projectile.GetComponent<Transform>().SetParent(GetComponent<Transform>(), true);
 
-			// Create the cable between the target and the projectile.
-			SpringJoint2D spring = gameObject.AddComponent<SpringJoint2D>();
-			spring.autoConfigureDistance = true;
-			spring.dampingRatio = 0f;
-			spring.frequency = 1f;
-			spring.connectedBody = launcher.GetComponent<Rigidbody2D>();
-			spring.enableCollision = true;
-			spring.autoConfigureDistance = false;
-			launcher.spring = spring;
+				// Create the cable between the target and the projectile.
+				SpringJoint2D spring = gameObject.AddComponent<SpringJoint2D>();
+				spring.autoConfigureDistance = true;
+				spring.dampingRatio = 0f;
+				spring.frequency = 1f;
+				spring.connectedBody = launcher.GetComponent<Rigidbody2D>();
+				spring.enableCollision = true;
+				spring.autoConfigureDistance = false;
+				launcher.spring = spring;
 
-			// Record the object that was hit.
-			launcher.connectedTarget = this;
+				// Record the object that was hit.
+				launcher.connectedTarget = this;
 
-			// Make the projectile no longer a projectile so that it doesn’t
-			// connect to a second target if this target moves close to the
-			// other.
-			Destroy(projectileComponent);
+				// Make the projectile no longer a projectile so that it
+				// doesn’t connect to a second target if this target moves
+				// close to the other.
+				Destroy(projectileComponent);
 
-			// Notify listeners.
-			onPlug.Invoke();
+				// Notify listeners.
+				onPlug.Invoke();
+			}
 		}
 	}
 }
