dbc1186fdc0d06a6cd4af7fcb4261f0c112e5006
Christopher Head <chead@chead.ca>
2021-06-13 08:06:54 -0700

    Clarify comment

--- Assets/Scripts/Catenary.cs
+++ Assets/Scripts/Catenary.cs
@@ -6,7 +6,8 @@ using UnityEngine.EventSystems;
 
 // Thanks to James O’Hare for the 3D catenary calculation code at
 // <https://gist.github.com/Farfarer/a765cd07920d48a8713a0c1924db6d70>. I
-// modified that work to change the calculation from 3D to 2D.
+// modified that work to change the calculation from 3D to 2D and extract it
+// into a stateless calculation function.
 public class Catenary {
 	public static Vector2[] generate(Vector2 start, Vector2 end, float length, int steps) {
 		float lineDist = Vector2.Distance(end, start);
