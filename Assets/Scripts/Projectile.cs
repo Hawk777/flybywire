690cf22df071e08d2168385f8874837f9aa2d07a
Christopher Head <chead@chead.ca>
2021-06-12 19:10:48 -0700

    Give the connector a visible cable while firing

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
+	[Tooltip("The cable part of the projectile.")]
+	public LineRenderer cable;
+
 	[HideInInspector]
 	public ComputerControls launcher;
+
+	private void Update() {
+		Vector3 projectilePos = GetComponent<Transform>().position;
+		Vector2 launcherPos = launcher.gameObject.GetComponent<Transform>().position;
+		cable.SetPositions(new Vector3[]{
+			projectilePos,
+			new Vector3(launcherPos.x, launcherPos.y, projectilePos.z),
+		});
+	}
 }
