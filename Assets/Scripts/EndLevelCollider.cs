a7e310dd5b6f74a8da3381cbb6d6ccc7f22390bb
Christopher Head <chead@chead.ca>
2021-06-12 23:14:57 -0700

    Delete unused variable

--- Assets/Scripts/EndLevelCollider.cs
+++ Assets/Scripts/EndLevelCollider.cs
@@ -2,17 +2,11 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class EndLevelCollider : MonoBehaviour {
-	[Tooltip("The objects to activate when the player touches this collider.")]
-	public GameObject[] objects;
-
 	[Tooltip("Emitted when the player touches this collider..")]
 	public UnityEvent onTouch;
 
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if(collision.gameObject.tag == "Player") {
-			foreach(GameObject i in objects) {
-				i.SetActive(true);
-			}
 			onTouch.Invoke();
 			Time.timeScale = 0f;
 		}
